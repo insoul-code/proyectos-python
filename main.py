e00ca64a1764d776d80c758ac92c6cc5d1d98fd3
CEIBA\santiago.restrepo <tiago931220@gmail.com>
2022-02-17 07:29:16 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#Comentario de línea
+""" Comentario
+de bloque """
+
+#salida por consola
+print("Hola mundo python, Gonorrea ome ⇩")
+
+#variables
+nombre = "Thiago"
+edad = 28
+estatura = 1.75
+eshinchaVerde = False
+
+print("Buenos días",nombre)
+
+print(f'Buenos días {nombre} sabemos que tienes {edad} años, tu estatura es {estatura}')
+
+#Recibiendo datos desde la consola
+
+num1 = int(input("Ingrese un número entero: "))
+num2 = int(input("Ingrese un número entero: "))
+resultado = num1+ num2
+print(f'La suma de los dos número es: {resultado}')
+
+
