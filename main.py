d9a9c987f5ba31bc5ab6a64f72d51fdc26c94692
CEIBA\santiago.restrepo <tiago931220@gmail.com>
2022-02-24 07:48:19 -0500

    Se actualiza archivo basado en la clase número 3 de nuevas tecnologías.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+''' 
 #Comentario de línea
 """ Comentario
 de bloque """
@@ -21,5 +22,54 @@ num1 = int(input("Ingrese un número entero: "))
 num2 = int(input("Ingrese un número entero: "))
 resultado = num1+ num2
 print(f'La suma de los dos número es: {resultado}')
+'''
+
+#Tomando decisiones
+'''
+numero = int(input("Ingrese un número:"))
+print(f'El número ingresado es: {numero}')
+
+if(numero >= 0):
+    print("El número es positivo")
+else:
+    print("El número es negativo")
+'''
+
+'''
+#Hidroituango
+nivelAgua = int(input("Digite el nivel de agua: "))
+if(nivelAgua < 200):
+    print("Hay muy poca agua en la represa")
+elif(nivelAgua >= 200 and nivelAgua < 450):
+    print("El nivel de agua es el adecuado")
+else:
+    print("Hay mucha agua, por favor abra las compuertas")
+'''
+
+'''
+#Estaciones
+
+mes = input("Digite cualquier mes: ")
+
+if(mes == "enero" or mes == "febrero" or mes == "marzo"):
+    print("Está en invierno")
+elif(mes == "abril" or mes == "mayo" or mes == "junio"):
+    print("Está en verano")
+elif(mes == "julio" or mes == "agosto" or mes == "septiembre"):
+    print("Está en otoño")
+elif(mes == "octubre" or mes == "noviembre" or mes == "diciembre"):
+    print("Está en primavera")
+else:
+    print("Ingresa un mes valido")
+'''
+
+#bucle
+contador = 0
+
+while(contador<10):
+    contador +=1
+    print(contador)
+else:
+    print("Termino el ciclo")
 
 
