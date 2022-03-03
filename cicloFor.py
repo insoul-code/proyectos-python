7de46f37e37b1e3c3c7a430240f71af86d0e86cd
CEIBA\santiago.restrepo <tiago931220@gmail.com>
2022-03-03 07:48:40 -0500

    Se realiza pruebas con ciclo for para recorrer listas.

---
+++ cicloFor.py
@@ -0,0 +1,12 @@
+'''for i in range (0, 20, 5):
+    print(i)'''
+
+#Trabajo con arreglos (Listas == Arreglos)
+
+nombres=["Juan","Carlos","Jose"]
+print(nombres)
+
+#Ciclo for para recorrer listas en python
+
+for nombre in nombres:
+    print(nombre)
