c9530b1fa98b33c38062c58ae5d2091515d138c2
CEIBA\santiago.restrepo <tiago931220@gmail.com>
2022-03-17 06:46:58 -0500

    Punto1: Se realiza la codificación del punto #1 para validar que numeros son multiplos de 2 y de 3.

---
+++ Examen/punto1.py
@@ -0,0 +1,17 @@
+#Pedir N números
+conteo = 0
+cantidadNumeros = int(input('Ingrese la cantidad de números a procesar: '))
+conteoMultiplos2 = 0
+conteoMultiplos3 = 0
+while(conteo < cantidadNumeros):
+    numero = int(input('Ingrese un número entero: '))
+    if(numero %2 == 0 and numero%3 ==0):
+        conteoMultiplos2 += 1
+        conteoMultiplos3 += 1
+    elif(numero %3 == 0):
+        conteoMultiplos3 += 1
+    elif(numero %2 == 0):
+        conteoMultiplos2 += 1
+    conteo+=1
+print(f'La cantidad de los números ingresados multiplos de 2 es: {conteoMultiplos2}')
+print(f'La cantidad de los números ingresados multiplos de 3 es: {conteoMultiplos3}')
